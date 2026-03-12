ca4fc9039e336461b57bbccfaaacbed03c448509
Edouard Rivain <edouardrivain@Edouards-Mac-mini.local>
2026-03-12 10:58:28 +0100

    feat: integrate Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { I18nProvider } from './hooks/i18nContext.tsx';
 import { AuthProvider } from './contexts/AuthContext.tsx';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <I18nProvider>
       <AuthProvider>
         <App />
+        <Analytics />
       </AuthProvider>
     </I18nProvider>
   </React.StrictMode>,
